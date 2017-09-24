6b8fce4bc86dd55957d6e9b173e6a8eae1ce5922
yuqinggen <2847779149@qq.com>
2017-09-24 15:25:30 +0800

    init project

---
+++ src/main/java/com/xry/GitAgainApplication.java
@@ -0,0 +1,12 @@
+package com.xry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitAgainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitAgainApplication.class, args);
+	}
+}
---
+++ src/main/java/com/xry/hello/HelloController.java
@@ -0,0 +1,19 @@
+package com.xry.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by xry_love on 2017/9/24.
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+
+    @GetMapping("/say")
+    public String sayHello(){
+        return "hello spring boot";
+    }
+}
